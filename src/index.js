dd7635f449cc4934bd4c0505824b3344292b456f
Paulina Sikora <paaulinaaas@gmail.com>
2019-01-26 18:20:48 +0100

    forms scss html fixes

--- src/index.js
+++ src/index.js
@@ -2,3 +2,6 @@ import style from './scss/main.scss';
 import flatpickr from "../node_modules/flatpickr";
 
 flatpickr("#datepicker", {});
+
+const button1 = Dropbox.createChooseButton(options);
+document.getElementById("container1").appendChild(button1);
