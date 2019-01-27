615d3f6793a8c1291077dbd424d93aa90edc5232
Paulina Sikora <paaulinaaas@gmail.com>
2019-01-27 20:14:42 +0100

    fixes

--- src/index.js
+++ src/index.js
@@ -3,17 +3,64 @@ import flatpickr from "flatpickr";
 
 flatpickr("#datepicker", {});
 
-// const button1 = Dropbox.createChooseButton(options);
-// document.getElementById("container1").appendChild(button1);
-
 const hamburger = document.querySelector('.hamburger');
 const menu = document.querySelector('.menu');
+const inputs = document.querySelectorAll('.form__input');
+const menuItem = document.querySelectorAll('.menu__item');
+const formInfo = document.getElementById('formInfo');
+const submitBtn = document.getElementById("submitBtn");
+const isIE = /*@cc_on!@*/false || !!document.documentMode;
+
+if(isIE) {
+    console.log('ok')
+}
 
 
-    hamburger.addEventListener('click', () => {
-        hamburger.classList.toggle('active')
-        menu.classList.toggle('active')
-
+//form validation
+inputs.forEach(input => {
+    input.addEventListener('focusout', ()=> {
+        if (!input.checkValidity() || input.value == '') {
+            input.closest('.form__field').classList.add('warning')
+        }
+        else {
+            input.closest('.form__field').classList.remove('warning')
+        }
     })
+})
 
-console.log('a')
+const isFormValid = () => {
+    for (let i = 0; i < inputs.length; i++ ) {
+        if (!inputs[i].checkValidity() || inputs[i].value == '') {
+            inputs[i].closest('.form__field').classList.add('warning')
+        }
+        else {
+            inputs[i].closest('.form__field').classList.remove('warning')
+        }
+    }
+    formInfo.classList.remove('show')
+}
+
+//closing menu after clicking on the link
+menuItem.forEach((item) => {
+    item.addEventListener('click', () => {
+        menu.classList.remove('active')
+    })
+})
+
+//menu open/close
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active')
+    menu.classList.toggle('active')
+})
+
+
+document.querySelector('.form').addEventListener('submit', event => {
+    event.preventDefault()
+    inputs.forEach((input) => {
+        input.value = ''
+    })
+    formInfo.classList.add('show')
+})
+
+submitBtn.addEventListener('click', isFormValid)
+
